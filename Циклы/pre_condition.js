c127bf082992531b2a29a3b81e6e9eeb336fdb01
Фуников Андрей Дмитриевич <funikov.1997@mail.ru>
2021-04-19 15:43:04 +0300

    Задачи на циклы, презентация и задания. Разбор задач и примеры

---
+++ Циклы/pre_condition.js
@@ -0,0 +1,20 @@
+/*Ввод предложения*/
+let string = prompt('Введите предложение');
+
+/*Разбираем предложение на массив слов через пробел*/
+/*
+let word_array = string.split(' ');
+let i = 0;
+while (i<word_array.length) {
+    alert(word_array[i]);
+    console.log(word_array[i]);
+    i++;
+}*/
+
+
+array = string.split('');
+console.log(array[0]);
+console.log(array[1]);
+console.log(array[2]);
+console.log(array[3]);
+
